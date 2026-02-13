f753b266f7015a2b800fed21db3046ca77f36b3f
Zaynab576 <zaynabmechnan12@gmail.com>
2026-02-13 13:35:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// ===== SCROLL REVEAL ANIMATION =====
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".section, .service-card").forEach(el => {
+    el.classList.add("hidden");
+    observer.observe(el);
+});
 
